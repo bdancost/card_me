53b509a291229bac56b9949c2cc674a83d2d4b11
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2024-12-10 16:10:37 -0300

    Adicionando melhorias no código

---
+++ script.js
@@ -0,0 +1,19 @@
+const diaIcon = document.querySelector(".icones_dia");
+const noiteIcon = document.querySelector(".icones_noite");
+
+// Função para ativar o tema claro
+function light() {
+  document.body.classList.remove("dark-mode");
+}
+
+// Função para ativar o tema escuro
+function dark() {
+  document.body.classList.add("dark-mode");
+}
+
+if (diaIcon && noiteIcon) {
+  diaIcon.addEventListener("click", light);
+  noiteIcon.addEventListener("click", dark);
+} else {
+  console.error("Ícones não encontrado no DOM");
+}
